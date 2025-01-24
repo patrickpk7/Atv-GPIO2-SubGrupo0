abf37962c90acaabbee0b0191db1f863bd79e177
Priscila S. <prisuzart@gmail.com>
2025-01-24 20:06:59 -0300

    Código com teclado matricial e 5 desenhos, luz branca

--- tarefa3_u4.c
+++ tarefa3_u4.c
@@ -16,9 +16,49 @@
 //pino de saída
 #define OUT_PIN 7
 
-//botão de interrupção
-const uint button_0 = 5;
-const uint button_1 = 6;
+//Teclado matricial
+
+uint linhas[4] = {1,2,3,4}; // Pinos das linhas do teclado matricial
+uint colunas[4] = {5,6,8,9}; // Pinos das colunas do teclado matricial
+
+
+///Matriz para mapear as teclas do teclado matricial 4x4 para os caracteres correspondentes a cada tecla pressionada (0-9, A-D, *, #)  
+char MAPA_TECLAS[4][4] = { 
+    '1', '2', '3', 'A',
+    '4', '5', '6', 'B',
+    '7', '8', '9', 'C',
+    '*', '0', '#', 'D'
+};
+
+//Função para inicializar os GPIOs do teclado matricial
+void inicia_teclado() {
+    for (int i = 0; i < 4; i++){ //Repete o bloco de código para os valores de i começando de 0 até 3 (inclusive), incrementando i em 1 a cada iteração
+        gpio_init(linhas[i]); //Inicializa o pino GPIO especificado no índice i do array linhas
+        gpio_set_dir(linhas[i], GPIO_OUT); //Configuração do modo de operação (direção) do pino GPIO indicado por linhas[i] como uma saída digital
+    }
+    for (int i = 0; i < 4; i++){ //Repete o bloco de código para os valores de i começando de 0 até 3 (inclusive), incrementando i em 1 a cada iteração
+        gpio_init(colunas[i]); //Inicializa o pino GPIO especificado no índice i do array colunas
+        gpio_set_dir(colunas[i], GPIO_IN); //Configuração do modo de operação (direção) do pino GPIO indicado por linhas[i] como uma entrada
+        gpio_pull_down(colunas[i]); //Ativa resistor de pull-down, quando nenhuma tecla estiver pressionada, o valor lido no pino será "0" (baixo)
+    } 
+}
+
+//Função para ler o teclado matricial (verifica se alguma tecla foi pressionada)
+char ler_teclado(){ //Declara a função chamada ler_teclado que retorna um valor do tipo char, a tecla pressionada
+    for (int i = 0; i < 4; i++){ //Repete o bloco de código para os valores de i começando de 0 até 3 (inclusive), incrementando i em 1 a cada iteração
+        gpio_put(linhas[i], 1); //Ativa a linha ( valor de 1 (ou HIGH) está sendo enviado para o pino GPIO especificado no índice i da array linhas)
+        for (int j = 0; j < 4; j++){ //Repete o bloco de código para os valores de j começando de 0 até 3 (inclusive), incrementando j em 1 a cada iteração
+    if (gpio_get(colunas[j])){ //Verifica se a coluna foi ativada
+        gpio_put(linhas[i],0); //Desativa a linha (valor de 0 (ou LOW) está sendo enviado para o pino GPIO especificado no índice i da array linhas)
+        sleep_ms(50); // Debounce (Espera 50 milissegundos para garantir que o sinal seja estável, evitando leituras falsas)
+        return MAPA_TECLAS[i][j]; //Retorna a tecla pressionada
+    }
+ }
+ gpio_put(linhas[i],0); ////Desativa a linha (valor de 0 (ou LOW) está sendo enviado para o pino GPIO especificado no índice i da array linhas)
+    }
+    return '\0'; //Retorna '\0' se nenhuma tecla foi pressionada
+}
+
 
 //vetores para desenhos na matriz de LED
 double seta_baixo[25] = {
@@ -107,31 +147,24 @@ int main() {
     uint sm = pio_claim_unused_sm(pio, true);
     pio_matrix_program_init(pio, sm, offset, OUT_PIN);
 
-    gpio_init(button_0);
-    gpio_set_dir(button_0, GPIO_IN);
-    gpio_pull_up(button_0);
-
-    gpio_init(button_1);
-    gpio_set_dir(button_1, GPIO_IN);
-    gpio_pull_up(button_1);
-
-    gpio_set_irq_enabled_with_callback(button_0, GPIO_IRQ_EDGE_FALL, 1, &gpio_irq_handler);
+    //Inicializa o teclado matricial
+    inicia_teclado();
+    
 
     // Vetor com os desenhos
     double *desenhos[5] = {seta_baixo, seta_cima, letra_x, quadrado, losango};
-    int desenho_atual = 0; // Índice do desenho atual
+    
 
     while (true) {
-        if (gpio_get(button_1)) {
-            // Exibe o desenho atual
-            desenho_pio(desenhos[desenho_atual], valor_led, pio, sm, r, g, b);
+       
+       char tecla = ler_teclado(); //Lê a tecla pressionada
 
-            // Avança para o próximo desenho
-            desenho_atual = (desenho_atual + 1) % 5;
-
-            // Pausa para que cada desenho seja visível antes de alternar
-            sleep_ms(500);
-        }
+       if(tecla == '0'){ //Verifica se a tecla '0' foi pressionada
+            for (int i = 0; i < 5; i++) {
+                desenho_pio(desenhos[i], valor_led, pio, sm, r, g, b);
+                sleep_ms(500); //Pausa entre os desenhos
+            }
+       }
     }
 }
 
